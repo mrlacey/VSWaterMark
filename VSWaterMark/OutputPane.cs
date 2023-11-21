c04fad66867465d03e8c456f035b99286113e7a7
Matt Lacey <matt@mrlacey.co.uk>
2023-11-21 00:23:07 +0000

    1.8.3 - Use a project-specific output pane

--- VSWaterMark/OutputPane.cs
+++ VSWaterMark/OutputPane.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GeneralOutputPane.cs" company="Matt Lacey Limited">
+﻿// <copyright file="OutputPane.cs" company="Matt Lacey Limited">
 // Copyright (c) Matt Lacey Limited. All rights reserved.
 // </copyright>
 
@@ -9,48 +9,48 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace VSWaterMark
 {
-    public class GeneralOutputPane
+    public class OutputPane
     {
-        private static GeneralOutputPane instance;
+        private static Guid wmPaneGuid = new Guid("45316C48-2EDB-499A-A5CB-3B6C118F8F37");
 
-        private readonly IVsOutputWindowPane generalPane;
+        private static OutputPane instance;
 
-        private GeneralOutputPane()
+        private readonly IVsOutputWindowPane pane;
+
+        private OutputPane()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var generalPaneGuid = VSConstants.GUID_OutWindowGeneralPane;
-
             if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
-             && (ErrorHandler.Failed(outWindow.GetPane(ref generalPaneGuid, out this.generalPane)) || this.generalPane == null))
+             && (ErrorHandler.Failed(outWindow.GetPane(ref wmPaneGuid, out this.pane)) || this.pane == null))
             {
-                if (ErrorHandler.Failed(outWindow.CreatePane(ref generalPaneGuid, "General", 1, 0)))
+                if (ErrorHandler.Failed(outWindow.CreatePane(ref wmPaneGuid, Vsix.Name, 1, 0)))
                 {
                     System.Diagnostics.Debug.WriteLine("Failed to create the Output window pane.");
                     return;
                 }
 
-                if (ErrorHandler.Failed(outWindow.GetPane(ref generalPaneGuid, out this.generalPane)) || (this.generalPane == null))
+                if (ErrorHandler.Failed(outWindow.GetPane(ref wmPaneGuid, out this.pane)) || (this.pane == null))
                 {
                     System.Diagnostics.Debug.WriteLine("Failed to get access to the Output window pane.");
                 }
             }
         }
 
-        public static GeneralOutputPane Instance => instance ?? (instance = new GeneralOutputPane());
+        public static OutputPane Instance => instance ?? (instance = new OutputPane());
 
         public void Activate()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            this.generalPane?.Activate();
+            this.pane?.Activate();
         }
 
         public void WriteLine(string message)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            this.generalPane?.OutputStringThreadSafe($"{message}{Environment.NewLine}");
+            this.pane?.OutputStringThreadSafe($"{message}{Environment.NewLine}");
         }
     }
 }
--- VSWaterMark/SponsorRequestHelper.cs
+++ VSWaterMark/SponsorRequestHelper.cs
@@ -26,23 +26,23 @@ namespace VSWaterMark
 
         private static void ShowThanksForSponsorshipMessage()
         {
-            GeneralOutputPane.Instance.WriteLine("Thank you for your sponsorship. It really helps.");
-            GeneralOutputPane.Instance.WriteLine("If you have ideas for new features or suggestions for new features");
-            GeneralOutputPane.Instance.WriteLine("please raise an issue at https://github.com/mrlacey/VSWaterMark/issues");
-            GeneralOutputPane.Instance.WriteLine(string.Empty);
+            OutputPane.Instance.WriteLine("Thank you for your sponsorship. It really helps.");
+            OutputPane.Instance.WriteLine("If you have ideas for new features or suggestions for new features");
+            OutputPane.Instance.WriteLine("please raise an issue at https://github.com/mrlacey/VSWaterMark/issues");
+            OutputPane.Instance.WriteLine(string.Empty);
         }
 
         private static void ShowPromptForSponsorship()
         {
-            GeneralOutputPane.Instance.WriteLine("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension (Watermark).");
-            GeneralOutputPane.Instance.WriteLine("I'm happy that the extensions I've created have been able to help you and many others");
-            GeneralOutputPane.Instance.WriteLine("but I also need to make a living, and two years without work and extended periods of illness have been a challenge. - I didn't qualify for any government support either. :(");
-            GeneralOutputPane.Instance.WriteLine(string.Empty);
-            GeneralOutputPane.Instance.WriteLine("Show your support by making a one-off or recurring donation at https://github.com/sponsors/mrlacey");
-            GeneralOutputPane.Instance.WriteLine(string.Empty);
-            GeneralOutputPane.Instance.WriteLine("If you become a sponsor, I'll tell you how to hide this message too. ;)");
-            GeneralOutputPane.Instance.WriteLine(string.Empty);
-            GeneralOutputPane.Instance.Activate();
+            OutputPane.Instance.WriteLine("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension (Watermark).");
+            OutputPane.Instance.WriteLine("I'm happy that the extensions I've created have been able to help you and many others");
+            OutputPane.Instance.WriteLine("but I also need to make a living, and two years without work and extended periods of illness have been a challenge. - I didn't qualify for any government support either. :(");
+            OutputPane.Instance.WriteLine(string.Empty);
+            OutputPane.Instance.WriteLine("Show your support by making a one-off or recurring donation at https://github.com/sponsors/mrlacey");
+            OutputPane.Instance.WriteLine(string.Empty);
+            OutputPane.Instance.WriteLine("If you become a sponsor, I'll tell you how to hide this message too. ;)");
+            OutputPane.Instance.WriteLine(string.Empty);
+            OutputPane.Instance.Activate();
         }
     }
 }
