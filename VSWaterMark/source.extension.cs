ac33939039ff5613eff799cbb18a28365f5b17fe
Matt Lacey <matt@mrlacey.co.uk>
2024-11-17 12:24:53 +0000

    v1.9

--- VSWaterMark/source.extension.cs
+++ VSWaterMark/source.extension.cs
@@ -11,7 +11,7 @@ namespace VSWaterMark
         public const string Name = "WaterMark";
         public const string Description = @"Add a configurable watermark to the editor.";
         public const string Language = "en-US";
-        public const string Version = "1.8.4";
+        public const string Version = "1.9";
         public const string Author = "Matt Lacey";
         public const string Tags = "";
     }
